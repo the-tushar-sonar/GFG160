26e3939d4f4e275a81b5362ed3926016bdf853e7
the-tushar-sonar <mylordtushar@gmail.com>
2026-06-08 14:48:18 +0000

    feat(day-003): solve Rotate Array with O(n^2)

--- 01_Array/04_Rotate_Array/my_solution.cpp
+++ 01_Array/04_Rotate_Array/my_solution.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+/*
 void rotateArr(vector<int> &arr, int d)
 {
     int n = arr.size();
@@ -13,12 +14,27 @@ void rotateArr(vector<int> &arr, int d)
 
     copy(temp.begin(), temp.end(), arr.begin() + (n - d));
 }
+*/
+
+void rotateArr(vector<int> &arr, int d)
+{
+    for (int i = 0; i < d; i++)
+    {
+        int n = arr.size();
+        int temp = arr[0];
+        for (int j = 1; j < n; j++)
+        {
+            arr[j - 1] = arr[j];
+        }
+        arr[n - 1] = temp;
+    }
+}
 
 int main()
 {
 
-    vector<int> arr = {1, 2, 3, 4, 5};
-    int d = 2;
+    vector<int> arr = {7, 3, 9, 1};
+    int d = 9;
 
     rotateArr(arr, d);
 
