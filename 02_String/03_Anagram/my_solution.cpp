2d61068d5ee5a69fdd9372cae482fc3895f2d451
the-tushar-sonar <mylordtushar@gmail.com>
2026-08-14 19:10:38 +0000

    feat(day-018): add my solution for problem Are Strings Anagrams

---
+++ 02_String/03_Anagram/my_solution.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool areAnagrams(string &s1, string &s2)
+{
+    sort(s1.begin(), s1.end());
+    sort(s2.begin(), s2.end());
+
+    if (s1.size() != s2.size())
+    {
+        // cout << "This not Anagram" << endl;
+        return false;
+    }
+    // int count = 0;
+    for (int i = 0; i <= s1.size(); i++)
+    {
+        if (s1[i] != s2[i])
+        {
+            // cout << "This not Anagram" << endl;
+            return false;
+        }
+        // count++;
+    }
+    // if (count == s1.size())
+        // cout << "This Anagram" << endl;
+        return true;
+}
+struct TestCase
+{
+    string s1, s2;
+    bool expected;
+};
+
+int main()
+{
+    // string s1 = "geeks";
+    // string s2 = "kaseg";
+
+    // areAnagrams(s1, s2);
+
+    vector<TestCase> tests = {
+        {"geeks", "kseeg", true},
+        {"allergy", "allergyy", false},
+        {"listen", "lists", false},
+        {"abc", "cba", true},
+        {"aabbc", "abbbc", false},
+        {"a", "a", true},
+        {"a", "b", false}};
+
+    for (auto &tc : tests)
+    {
+        bool result = areAnagrams(tc.s1, tc.s2);
+
+        if (result == tc.expected)
+            cout << "PASS\n";
+        else
+            cout << "FAIL | Expected: " << tc.expected
+                 << " | Got: " << result << '\n';
+    }
+
+    return 0;
+}
