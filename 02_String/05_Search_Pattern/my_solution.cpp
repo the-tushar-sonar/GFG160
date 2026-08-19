91b3a60dd4d24db1142622e22a5d8856f89839e6
the-tushar-sonar <mylordtushar@gmail.com>
2026-08-19 17:56:53 +0000

    feat(day-023): add optimized solution using KMP Algorithm at O(n+m) time complexity

--- 02_String/05_Search_Pattern/my_solution.cpp
+++ 02_String/05_Search_Pattern/my_solution.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+/*
 // Brute Force => O(n*m)
 vector<int> search(string &pat, string &txt)
 {
@@ -49,27 +50,98 @@ vector<int> search(string &pat, string &txt)
 
     // for (int num : result)
     // {
-    //     cout << num << " ";
-    // }
+        //     cout << num << " ";
+        // }
 
-    // cout << endl;
+        // cout << endl;
+        return result;
+    }
+*/
+
+// Using KMP Algorithm & LPS => O(n+m)
+void constructLps(string &pat, vector<int> &lps)
+{
+    int len = 0;
+    lps[0] = 0;
+
+    int i = 1;
+    while (i < pat.length())
+    {
+        if (pat[i] == pat[len])
+        {
+            len++;
+            lps[i] = len;
+            i++;
+        }
+        else
+        {
+            if (len != 0)
+            {
+                len = lps[len - 1];
+            }
+            else
+            {
+                lps[i] = 0;
+                i++;
+            }
+        }
+    }
+}
+
+vector<int> search(string &pat, string &txt)
+{
+    int n = txt.length();
+    int m = pat.length();
+
+    vector<int> lps(m);
+    vector<int> result;
+
+    constructLps(pat, lps);
+
+    int i = 0;
+    int j = 0;
+
+    while (i < n)
+    {
+        if (txt[i] == pat[j])
+        {
+            i++;
+            j++;
+
+            if (j == m)
+            {
+                result.push_back(i - j);
+                j = lps[j - 1];
+            }
+        }
+        else
+        {
+            if (j != 0)
+            {
+                j = lps[j - 1];
+            }
+            else
+            {
+                i++;
+            }
+        }
+    }
+
     return result;
 }
+
 int main()
 {
     // string txt = "aacabdc";
     // string pat = "aab";
     // string txt = "geeksforgeeks";
     // string pat = "geek";
-    string txt = "aaaaa";
-    string pat = "aa";
+    string txt = "aabaacaadaabaaba";
+    string pat = "aaba";
 
-    vector<int> ans = search(pat, txt);
-
-    for (int n : ans)
-    {
-        cout << n << " ";
-    }
+    vector<int> res = search(pat, txt);
+    for (int i = 0; i < res.size(); i++)
+        cout << res[i] << " ";
     cout << endl;
 
     return 0;
